c76a4bf2ef015f9654306f8e152d81bcb48e3163
ARNAB PAL <arnab.abstentia@gmail.com>
2019-05-21 13:33:13 +0530

    Renamed the num_rows to num_cols, updated out path

--- generate_randomid.py
+++ generate_randomid.py
@@ -1,5 +1,7 @@
 import argparse
 import random
+from os.path import join
+from settings import output_dir
 
 def generate_radom_uid(num_uuid, len_uuid):
     chars = list('abcdefghijklmnopqrstuvwxyz0123456789')
@@ -14,23 +16,25 @@ if __name__=="__main__":
     
     argument_parser.add_argument('--num_uuid', type=int, default=20_000_000)
     argument_parser.add_argument('--len_uuid', type=int, default=8)
-    argument_parser.add_argument('--num_rows', type=int, default=20)
+    argument_parser.add_argument('--num_cols', type=int, default=20)
     argument_parser.add_argument('--csv_fname', type=str, default='random_uuid.csv')
 
     parse_args = argument_parser.parse_args()
 
     num_uuid = parse_args.num_uuid
     len_uuid = parse_args.len_uuid
-    num_rows = parse_args.num_rows
+    num_cols = parse_args.num_cols
     csv_fname = parse_args.csv_fname
     
+    csv_fpath = join(output_dir, csv_fname)
+
     uid_container = list(generate_radom_uid(num_uuid, len_uuid))
     # creating batch of 20 rows
-    container_batch = [uid_container[x:x+num_rows] for x in range(0, len(uid_container), num_rows)]
+    container_batch = [uid_container[x:x+num_cols] for x in range(0, len(uid_container), num_cols)]
     # reducing memory footprint by deleting the set object
     del uid_container
     # writing the csv file
-    with open(csv_fname, 'w') as fd:
+    with open(csv_fpath, 'w') as fd:
         while len(container_batch) > 0:
             batch = container_batch.pop()
             fd.write(','.join(batch))
